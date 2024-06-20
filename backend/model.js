6fc5fc79c34604154f3d974579e95ba94c49367c
Nomesh88 <Nomeshpalakaluri@gmail.com>
2024-06-21 00:03:35 +0530

    Basic To-do

---
+++ backend/model.js
@@ -0,0 +1,26 @@
+//todoList.js
+
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    priority:{
+        type: String,
+        required:true,
+    },
+    deadline: {
+        type: Date,
+    },
+});
+
+
+const todoList = mongoose.model("todo", todoSchema);
+
+module.exports = todoList;
